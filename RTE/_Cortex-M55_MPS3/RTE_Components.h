eee6e40aec06a75662fb74942e7a14f62f61bc80
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2022-02-14 16:42:17 +0000

    use the latest perf_counter

--- RTE/_Cortex-M55_MPS3/RTE_Components.h
+++ RTE/_Cortex-M55_MPS3/RTE_Components.h
@@ -18,18 +18,18 @@
 
 /* ARM::CMSIS Driver:USART:1.0.0 */
 #define RTE_Drivers_USART
-/* GorgonMeducer.Performance Counter::Utilities:perf_counter:Core:Library:1.9.1 */
+/* GorgonMeducer.Performance Counter::Utilities:perf_counter:Core:Library:1.9.2 */
 #define __PERF_COUNTER__
 /* Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
 #define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
           #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
-/* LVGL.LVGL::LVGL:lvgl:Benchmark:8.2.0 */
+/* LVGL.LVGL::LVGL:lvgl:Benchmark:8.3.0-dev */
 /*! \brief enable demo:bencharmk */
 #define LV_USE_DEMO_BENCHMARK         1
-/* LVGL.LVGL::LVGL:lvgl:Essential:8.2.0 */
+/* LVGL.LVGL::LVGL:lvgl:Essential:8.3.0-dev */
 /*! \brief Enable LVGL */
 #define RTE_GRAPHICS_LVGL
-/* LVGL.LVGL::LVGL:lvgl:Extra Themes:8.2.0 */
+/* LVGL.LVGL::LVGL:lvgl:Extra Themes:8.3.0-dev */
 /*! \brief use extra themes, widgets and layouts */
 #define RTE_GRAPHICS_LVGL_USE_EXTRA_THEMES
 
