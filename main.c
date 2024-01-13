1b6699cb1570733a7800cafe975732ee1c8b3e27
Gabriel Wang <gabriel.wang@arm.com>
2024-01-13 17:13:59 +0000

    minor update

--- main.c
+++ main.c
@@ -84,7 +84,7 @@ int main(void)
     
     //lv_demo_benchmark_run_scene(LV_DEMO_BENCHMARK_MODE_RENDER_AND_DRIVER, 26*2-1);      // run scene no 31
 #elif LV_USE_DEMO_RENDER
-    lv_demo_render(LV_DEMO_RENDER_SCENE_IMAGE_RECOLOR, 128);
+    lv_demo_render(LV_DEMO_RENDER_SCENE_IMAGE_NORMAL, 128);
 #elif LV_USE_DEMO_WIDGETS
     lv_demo_widgets();
 #elif LV_USE_DEMO_MUSIC
